1b3e3b3cd9410d4250e3b804bd2f152d9f5700c3
EliteDudeIT <titanfrax@gmail.com>
2024-03-17 18:10:13 +0200

    add hashed

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import bcrypt
 import asyncio
 import logging
 
@@ -43,14 +44,21 @@ async def register_login(message: types.Message, state: FSMContext):
 
 @router.message(Steps.register, F.text)
 async def register(message: types.Message, state: FSMContext):
-    new_password = Password(id=message.chat.id, password=message.text)
+    # new_password = Password(id=message.chat.id, password=message.text)
+    # session.add(new_password)
+    # session.commit()
+
+    hash_password = bcrypt.hashpw(message.text.encode(), bcrypt.gensalt())
+    new_password = Password(id=message.chat.id, password=hash_password)
+    print(hash_password)
     session.add(new_password)
     session.commit()
+
 @router.message(Steps.login, F.text)
 async def login(message: types.Message, state: FSMContext):
     password_check = session.query(Password).filter(Password.id == message.chat.id).first()
     if password_check:
-        if password_check.password == message.text:
+        if bcrypt.checkpw(message.text.encode(), password_check.password):
             await message.answer('Пароль і логін вірні')
             await message.answer('Обирайте Кнопку для зберігання паролю', reply_markup=mes)
             await state.set_state(Steps.messengers)
@@ -68,40 +76,37 @@ async def git(message: types.Message, state: FSMContext):
     git_login = message.text
     print(git_login)
     await message.answer('Введіть пароль')
-    # git_password = message.text
-    # print(git_password)
+    await state.set_state(Steps.git_password)
+@router.message(Steps.git_password, F.text)
+async def git_password(message: types.Message, state: FSMContext):
+    git_password = message.text
+    print('git password', git_password)
 @router.callback_query(Steps.messengers, F.data)
-async def messengers(callback: types.CallbackQuery, state: FSMContext):
+async def messengers2(callback: types.CallbackQuery, state: FSMContext):
     if callback.data == 'dis':
         await callback.message.answer('Введіть логін для Discord')
         await state.set_state(Steps.git)
 
 @router.callback_query(Steps.messengers, F.data)
-async def messengers(callback: types.CallbackQuery, state: FSMContext):
+async def messengers3(callback: types.CallbackQuery, state: FSMContext):
     if callback.data == 'gygl':
         await callback.message.answer('Введіть логін для Google')
         await state.set_state(Steps.git)
 
 @router.callback_query(Steps.messengers, F.data)
-async def messengers(callback: types.CallbackQuery, state: FSMContext):
-    if callback.data == 'mail':
-        await callback.message.answer('Введіть логін для Gmail')
-        await state.set_state(Steps.git)
-
-@router.callback_query(Steps.messengers, F.data)
-async def messengers(callback: types.CallbackQuery, state: FSMContext):
+async def messengers4(callback: types.CallbackQuery, state: FSMContext):
     if callback.data == 'fb':
         await callback.message.answer('Введіть логін для FoceBook')
         await state.set_state(Steps.git)
 
 @router.callback_query(Steps.messengers, F.data)
-async def messengers(callback: types.CallbackQuery, state: FSMContext):
+async def messengers5(callback: types.CallbackQuery, state: FSMContext):
     if callback.data == 'stm':
         await callback.message.answer('Введіть логін для Steam')
         await state.set_state(Steps.git)
 
 @router.callback_query(Steps.messengers, F.data)
-async def messengers(callback: types.CallbackQuery, state: FSMContext):
+async def messengers6(callback: types.CallbackQuery, state: FSMContext):
     if callback.data == 'tvtw':
         await callback.message.answer('Введіть логін для Twitch')
         await state.set_state(Steps.git)
